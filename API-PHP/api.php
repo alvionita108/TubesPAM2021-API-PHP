40ab3805f8f0606bc0b703bd1f2df8d5863661a6
cita siadari <43009444+citasiadari@users.noreply.github.com>
2021-05-07 20:30:40 +0700

    add function update and delete user

--- API-PHP/api.php
+++ API-PHP/api.php
@@ -13,6 +13,8 @@ switch($op){
     case '' : normal(); break;    
     case 'create' : create(); break;
     case 'detail' : detail() ; break;
+    case 'update' : update() ; break;
+    case 'delete' : delete() ; break;
     default : normal(); break;
 }
 
@@ -40,18 +42,18 @@ function normal(){
 //function to create or add new user
 function create(){
     global $koneksi;
-    $nama_lengkap = $_POST['nama_lengkap']; 
-    $username = $_POST['username'];
-    $password = $_POST['password']; 
-    $email = $_POST['email'];  
+    $username = $_POST['username']; 
+    $email = $_POST['email'];
+    $password = $_POST['password'];
     $alamat = $_POST['alamat'];  
     $jenis_kelamin = $_POST['jenis_kelamin'];
     $no_telp = $_POST['no_telp'];
     $foto_profil = $_POST['foto_profil'];
+    $nama_lengkap = $_POST['nama_lengkap']; 
 
     $hasil = "Gagal menambah data";
-    if($username and $email and $password){
-        $sql1 = "insert into user(nama_lengkap, username, password,email, alamat, jenis_kelamin, no_telp, foto_profil) values ('$username',  '$password', '$email','$alamat', '$jenis_kelamin', '$no_telp', '$foto_profil')";
+    if($username and $email and $password and $alamat and $jenis_kelamin and $no_telp and $foto_profil and $nama_lengkap){
+        $sql1 = "insert into user(username, password,email, nama_lengkap, alamat, jenis_kelamin, no_telp, foto_profil) values ('$username', '$password' ,'$email', '$nama_lengkap', '$alamat', '$jenis_kelamin', '$no_telp', '$foto_profil')";
         $q1 = mysqli_query($koneksi, $sql1);
         if($q1){
             $hasil='Berhasil menambah data';
@@ -64,7 +66,7 @@ function create(){
 //function to get detail user
 function detail(){
     global $koneksi;
-     $username = $_GET['username'];
+    $username = $_GET['username'];
 
     $sql1 = "select * from user where username = '$username'";
     $q1 = mysqli_query($koneksi, $sql1);
@@ -85,6 +87,77 @@ function detail(){
     echo json_encode($data);
 }
 
+// // function to update data user
+function update(){
+    global $koneksi;    
+    $username = $_GET['username'];
+    $nama_lengkap = $_POST['nama_lengkap'];   
+    $password = $_POST['password']; 
+    $email = $_POST['email'];  
+    $alamat = $_POST['alamat'];  
+    $jenis_kelamin = $_POST['jenis_kelamin'];
+    $no_telp = $_POST['no_telp'];
+    $foto_profil = $_POST['foto_profil'];
+
+  
+    if ($nama_lengkap){
+        $set[] = "nama_lengkap='$nama_lengkap'";
+    }
+
+    if ($password){
+        $set[] = "password='$password'";
+    }
+
+    if ($email){
+        $set[] = "email='$email'";
+    }
+
+    if ($alamat){
+        $set[] = "alamat='$alamat'";
+    }
+
+    if ($jenis_kelamin){
+        $set[] = "jenis_kelamin='$jenis_kelamin'";
+    }
+
+    if ($no_telp){
+        $set[] = "no_telp='$no_telp'";
+    }
+
+    if ($foto_profil){
+        $set[] = "foto_profil='$foto_profil'";
+    }
+
+    $hasil = " Gagal mengupdate data";
+    if($nama_lengkap or $password or $alamat or $email or $jenis_kelamin or $no_telp or $foto_profil){
+        $sql1 = "update user set ".implode(",",$set). "where username = '$username'";
+        $q1 = mysqli_query($koneksi, $sql1);
+        if($q1){
+            $hasil='Data telah diupdate';
+        }
+    }
+
+    $data['data']['result'] = $hasil;
+    echo json_encode($data);
+}
+
+//menghapus data user 
+function delete(){
+    global $koneksi;
+    $username = $_GET['username'];
+    $sql1 = "Delete from user where username = '$username'";
+    $q1 = mysqli_query($koneksi, $sql1);
+
+   
+    if($sq1){
+        $hasil = "Data telah berhasil dihapus";
+    }else{
+        $hasil = "Data gagal dihapus";
+    }
+
+    $data['data']['result'] = $hasil;
+    echo json_encode($data);
+}
 
     
 
