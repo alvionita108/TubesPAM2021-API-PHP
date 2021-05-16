7bd0324f15e280215657fa068c91c5cb71acb02c
alvionita108 <60423164+alvionita108@users.noreply.github.com>
2021-05-16 23:07:17 +0700

    Add files via upload

--- API-PHP/danustera.sql
+++ API-PHP/danustera.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 07, 2021 at 01:14 PM
+-- Generation Time: May 16, 2021 at 04:04 PM
 -- Server version: 5.6.20-log
 -- PHP Version: 5.4.31
 
@@ -27,14 +27,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `makanan` (
+  `id_user` int(11) NOT NULL,
 `id_makanan` int(11) NOT NULL,
-  `jenis_makanan` varchar(100) NOT NULL,
-  `nama_makanan` varchar(255) NOT NULL,
+  `nama_makanan` varchar(50) NOT NULL,
+  `deskripsi_makanan` varchar(250) NOT NULL,
   `foto_makanan` varchar(255) NOT NULL,
   `stok_harian` int(30) NOT NULL,
   `harga_satuan` int(30) NOT NULL,
-  `username` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `nama_lengkap` varchar(25) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_telp` varchar(15) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 -- --------------------------------------------------------
 
@@ -43,15 +46,15 @@ CREATE TABLE IF NOT EXISTS `makanan` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
+`id_user` int(11) NOT NULL,
   `nama_lengkap` varchar(255) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `password` varchar(8) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL,
-  `jenis_kelamin` varchar(9) NOT NULL,
-  `no_telp` varchar(20) NOT NULL,
-  `foto_profil` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `jenis_kelamin` enum('0','1') NOT NULL,
+  `no_telp` varchar(15) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=72 ;
 
 --
 -- Indexes for dumped tables
@@ -61,13 +64,13 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Indexes for table `makanan`
 --
 ALTER TABLE `makanan`
- ADD PRIMARY KEY (`id_makanan`), ADD KEY `username` (`username`);
+ ADD PRIMARY KEY (`id_makanan`), ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`username`);
+ ADD PRIMARY KEY (`id_user`), ADD UNIQUE KEY `email` (`email`), ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -77,7 +80,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `makanan`
 --
 ALTER TABLE `makanan`
-MODIFY `id_makanan` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_makanan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=72;
 --
 -- Constraints for dumped tables
 --
@@ -86,7 +94,7 @@ MODIFY `id_makanan` int(11) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `makanan`
 --
 ALTER TABLE `makanan`
-ADD CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
