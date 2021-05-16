06642b264910a5e1fb4430555f2b67fb4d2e96e1
Yuni Nurdiyanti <64515983+yuni118140044@users.noreply.github.com>
2021-05-16 20:38:32 +0700

    Update buatAkun.php

--- API-PHP/buatAkun.php
+++ API-PHP/buatAkun.php
@@ -1,6 +1,7 @@
 <?php
 include_once "koneksi.php";
 $data = json_decode(file_get_contents('php://input'), true);
+
 $username = $data['username'];
 $nama_lengkap = $data['nama_lengkap'];
 $email = $data['email'];
@@ -8,8 +9,10 @@ $alamat = $data['alamat'];
 $jenis_kelamin = $data['jenis_kelamin'];
 $no_telp = $data['no_telp'];
 $password = $data['password'];
+
 $sql = "insert into user(username, password,email, nama_lengkap, alamat, jenis_kelamin, no_telp) values ('$username', '$password' ,'$email', '$nama_lengkap', '$alamat', '$jenis_kelamin', '$no_telp')";
 $info = array();
+
 $info['sql'] = $sql;
 if (mysqli_query($koneksi, $sql)) {
 	$info = "Akun berhasil di daftarkan";
